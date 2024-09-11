a8c370381bd63461796e55e1d24a3a1768395a8c
Nikita Antonenkov <nikita2102antonenkov at gmail dot com>
2024-09-11 18:12:08 +0300

    Fixed

--- lab1.py
+++ lab1.py
@@ -2,10 +2,17 @@ import csv
 import os
 import numpy
 from tabulate import tabulate
-from typing import TextIO
 
 
 MAX_FILE_SIZE_MB = 1024
+
+
+class Statistics:
+    def __init__(self, min: float, max: float, median: float, mean: float):
+        self.min = min
+        self.max = max
+        self.median = median
+        self.mean = mean
 
 
 class FileExtensionError(ValueError):
@@ -24,7 +31,11 @@ class FieldMismatchError(ValueError):
     pass
 
 
-class RegionNotExistsError(ValueError):
+class RegionNumberError(ValueError):
+    pass
+
+
+class EmptyFieldError(ValueError):
     pass
 
 
@@ -37,20 +48,26 @@ def validate_file(filename: str) -> None:
         raise FileSizeError('File is too large')
 
 
-def read_column_number() -> int:
+def read_column_number(column_count: int) -> int:
     try:
         column_number = int(input())
+        if column_number >= column_count or column_number < 0:
+            raise ColumnNumberError
     except ValueError:
         raise ColumnNumberError
 
     return column_number
 
 
-def validate_column_number(column_number: int, column_count: int) -> int:
-    if column_number >= column_count or column_number < 0:
-        raise ColumnNumberError
+def read_region_number(region_count: int) -> int:
+    try:
+        region_number = int(input())
+        if region_number < 0 or region_number >= region_count:
+            raise RegionNumberError
+    except ValueError:
+        raise RegionNumberError
 
-    return column_number
+    return region_number
 
 
 def validate_row(row: list[str], column_count: int) -> list[str]:
@@ -64,36 +81,51 @@ def extract_column_values(rows: list[list[str]], column_number: int, column_coun
     values = []
     for row in rows:
         validate_row(row, column_count)
-        if not row[column_number]:
-            continue
         try:
+            if not row[column_number]:
+                raise EmptyFieldError
             value = float(row[column_number])
+        except EmptyFieldError:
+            print(f'Error: Empty field')
+            continue
         except ValueError:
             print(f'Error: Invalid value "{row[column_number]}"')
             continue
         values.append(value)
 
+    if not values:
+        raise ValueError('No valid values found')
+
     return values
 
 
-def extract_rows(file: TextIO, column_count: int) -> list[list[str]]:
-    rows = []
-    for row in csv.reader(file):
+def extract_unique_sorted_regions(rows: list[list[str]]) -> list[str]:
+    unique_regions = sorted({row[1] for row in rows[1:]})
+    
+    return unique_regions
+
+
+def validate_rows(rows, column_count: int) -> None:
+    for row in rows:
         validate_row(row, column_count)
-        rows.append(row)
-
-    return rows
 
 
-def to_region_filtered_rows(region: str, rows: list[list[str]]) -> list[list[str]]:
-    filtered_rows = list(filter(lambda row: row[1] == region, rows))
+def to_region_filtered_rows(region_number: int, rows: list[list[str]]) -> list[list[str]]:
+    unique_regions = extract_unique_sorted_regions(rows)
+
+    if region_number >= len(unique_regions) or region_number < 0:
+        raise RegionNumberError(f"Region with index {region_number} doesn't exist")
+    
+    selected_region = unique_regions[region_number]
+    
+    filtered_rows = list(filter(lambda row: row[1] == selected_region, rows))
+    
     if not filtered_rows:
-        raise RegionNotExistsError(region)
+        raise RegionNumberError(f"No rows found for region: {selected_region}")
 
     return filtered_rows
 
-
-def calculate_statistics(values: list[float]) -> tuple[float, float, float, float]:
+def calculate_statistics(values: list[float]) -> Statistics:
     values.sort()
 
     min = values[0]
@@ -102,19 +134,19 @@ def calculate_statistics(values: list[float]) -> tuple[float, float, float, floa
         else (values[len(values) // 2] + values[len(values) // 2 - 1]) / 2)
     mean = sum(values) / len(values)
 
-    return min, max, median, mean
+    return Statistics(min=min, max=max, median=median, mean=mean)
 
 
 def print_rows(rows: list[list[str]], header: list[str]) -> None:
     print(tabulate(rows, headers=header, tablefmt='fancy_grid'))
 
 
-def print_statistics(min: float, max: float, median: float, mean: float) -> None:
+def print_statistics(stat: Statistics) -> None:
     print(tabulate(
-        [['Min', min],
-         ['Max', max],
-         ['Median', median],
-         ['Mean', mean]],
+        [['Min', stat.min],
+         ['Max', stat.max],
+         ['Median', stat.median],
+         ['Mean', stat.mean]],
          headers=['Statistics', 'Value']))
     print('\n')
 
@@ -124,47 +156,89 @@ def print_perecentile(values: list[float]) -> None:
     print(tabulate(lst, headers=['Percentile', 'Value']))   
 
 
-print('CSV file name: ')
+def print_regions_with_indices(regions: list[str]) -> None:
+    indexed_regions = [(index, region) for index, region in enumerate(regions)]
+    print(tabulate(indexed_regions, headers=["Index", "Region"], tablefmt='grid'))
 
-filename = input()
 
-try:
+def print_columns_with_indices(header: list[str]) -> None:
+    indexed_columns = [(index, column) for index, column in enumerate(header)]
+    print(tabulate(indexed_columns, headers=["Index", "Column"], tablefmt='grid'))    
+
+
+def read_file(filename: str) -> list[list[str]]:
     with open(filename, 'r', encoding='utf-8') as file:
         validate_file(filename)
 
-        print('Region name: ')
-
-        region = input()
-
-        print('Column No: ')
-
-        column_number = read_column_number()
-        header = csv.reader(file).__next__()
+        rows = [row for row in csv.reader(file)]
+        header = rows[0]
         column_count = len(header)
 
-        validate_column_number(column_number, column_count)
+        validate_rows(rows, column_count)
 
-        filtered_rows = to_region_filtered_rows(region, extract_rows(file, column_count))
-        values = extract_column_values(filtered_rows, column_number, column_count)
-        min, max, median, mean = calculate_statistics(values)
+        return rows
 
-        print_rows(filtered_rows, header)
-        print_statistics(min, max, median, mean)
-        print_perecentile(values)
 
-except FileNotFoundError:
-    print('Error: File not found')
-except PermissionError:
-    print('Error: Permission denied')
-except FileExtensionError:
-    print('Error: Invalid file extension')
-except FileSizeError as e:
-    print(f'Error: {e}')
-except ColumnNumberError:
-    print('Error: Invalid column number')
-except FieldMismatchError:
-    print('Error: Field mismatch')
-except RegionNotExistsError as e:
-    print(f'Error: Region "{e}" doesn\'t exists')
-except Exception as e:
-    print(f'Error: {e}')
+def select_region_number(rows: list[list[str]]) -> int:
+    unique_regions = extract_unique_sorted_regions(rows)
+
+    print_regions_with_indices(unique_regions)
+    print('Region No: ')
+
+    return read_region_number(len(unique_regions))
+
+
+def select_column_number(rows: list[list[str]]) -> int:
+    header = rows[0]
+    column_count = len(header)
+
+    print_columns_with_indices(header)
+    print('Column No: ')
+
+    return read_column_number(column_count)
+
+
+def process_statistics(rows: list[list[str]], region_number: int, column_number: int) -> None:
+    filtered_rows = to_region_filtered_rows(region_number, rows)
+    header = rows[0]
+    column_count = len(header)
+    values = extract_column_values(filtered_rows, column_number, column_count)
+    stat = calculate_statistics(values)
+
+    print_rows(filtered_rows, header)
+    print_statistics(stat)
+    print_perecentile(values)
+ 
+
+def main():
+    print('CSV file name: ')
+
+    filename = input()
+
+    try:
+        rows = read_file(filename)
+        region_number = select_region_number(rows)
+        column_number = select_column_number(rows)
+
+        process_statistics(rows, region_number, column_number)
+
+    except FileNotFoundError:
+        print('Error: File not found')
+    except PermissionError:
+        print('Error: Permission denied')
+    except FileExtensionError:
+        print('Error: Invalid file extension')
+    except FileSizeError as e:
+        print(f'Error: {e}')
+    except ColumnNumberError:
+        print('Error: Invalid column number')
+    except FieldMismatchError:
+        print('Error: Field mismatch')
+    except RegionNumberError as e:
+        print(f'Error: Invalid region number')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+if __name__ == '__main__':
+    main()
